1a9f59ceb75ea5f0160be5684230b863e678ec8e
Xhemajl Kabashi <44348785+xhemmajl@users.noreply.github.com>
2020-07-15 23:59:45 +0200

    Update OnlineUsersTableViewController.swift

--- Todo/Todo/OnlineUsersTableViewController.swift
+++ Todo/Todo/OnlineUsersTableViewController.swift
@@ -5,3 +5,6 @@
 //  Created by Valton Sherifi on 15/07/2020.
 //  Copyright © 2020 Valton Sherifi. All rights reserved.
 //
+import UIKit
+
+class OnlineUsersTableViewController: UITableViewController {
