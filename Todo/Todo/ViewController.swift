e4a047076123c6c23fe42c745664193dce17ff51
Valton <valtoni.gl@live.com>
2020-07-15 22:48:54 +0200

    Krijimi i Projektit

---
+++ Todo/Todo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Djellza Rrustemi on 7/15/20.
+//  Copyright © 2020 Valton Sherifi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
