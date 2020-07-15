ba0cfe3344956f343d59fdea54ea31e82c161dc5
Xhemajl Kabashi <44348785+xhemmajl@users.noreply.github.com>
2020-07-15 23:10:58 +0200

    Update LoginViewController.swift

--- Todo/Todo/LoginViewController.swift
+++ Todo/Todo/LoginViewController.swift
@@ -1,9 +1,28 @@
 //
 //  LoginViewController.swift
-//  Todo
+//  TODO
 //
-//  Created by Djellza Rrustemi on 7/15/20.
+//  Created by Valton Sherifi on 7/10/20.
 //  Copyright © 2020 Valton Sherifi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Constants
+    let loginToList = "LoginToList"
+    
+    // MARK: Outlets
+    @IBOutlet weak var textFieldLoginEmail: UITextField!
+    @IBOutlet weak var textFieldLoginPassword: UITextField!
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    // MARK: UIViewController Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
