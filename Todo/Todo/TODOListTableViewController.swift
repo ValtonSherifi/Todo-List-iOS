8c90c9822efc60cbd216f0401ef9e6e4e518fdd5
Xhemajl Kabashi <44348785+xhemmajl@users.noreply.github.com>
2020-07-19 00:16:43 +0200

    Update TODOListTableViewController.swift

--- Todo/Todo/TODOListTableViewController.swift
+++ Todo/Todo/TODOListTableViewController.swift
@@ -1,176 +1,80 @@
 //
-//  TODOListTableViewController.swift
+//  OnlineUsersTableViewController.swift
 //  TODO
 //
 //  Created by Djellza Rrustemi on 7/15/20.
 //  Copyright © 2020 Valton Sherifi. All rights reserved.
 //
-
 import UIKit
 import Firebase
 
-class TODOListTableViewController: UITableViewController {
-
+class OnlineUsersTableViewController: UITableViewController {
+  
   // MARK: Constants
-  let listToUsers = "ListToUsers"
+  let userCell = "UserCell"
   
   // MARK: Properties
-  var items: [TodoItem] = [] // incializimi i modelit per listen e te dhenave Todo
-    var user: User! // inicializimi i modelit te User-ave
-  var userCountBarButtonItem: UIBarButtonItem! // Inicializimi i butonit ne navigation
-    
-  //Konektimi me Databaze
-  let ref = Database.database().reference(withPath: "todo-items") // krijimi i tabeles todo-items ne firebase
-  let usersRef = Database.database().reference(withPath: "online") // krijimi i tabeles online per perdorues  ne firebase
+  var currentUsers: [String] = []
+  let usersRef = Database.database().reference(withPath: "online")
   
   override var preferredStatusBarStyle: UIStatusBarStyle {
     return .lightContent
   }
-
+  
+  // MARK: UIViewController Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    tableView.allowsMultipleSelectionDuringEditing = false // Lejimi i selektimit te rreshtit ne tabele
-    // Bashkangjitja e funksionalitetit ne butonin per numerimin e userave
-    userCountBarButtonItem = UIBarButtonItem(title: "1",
-                                             style: .plain,
-                                             target: self,
-                                             action: #selector(userCountButtonDidTouch))
-    userCountBarButtonItem.tintColor = UIColor.white // Ngjyrosja e butonit ne navigation bar
-    navigationItem.leftBarButtonItem = userCountBarButtonItem // Vendosja butonit te krijuar manualisht(userCountBarButtonItem) ne butonin e navigimit leftbuttonItem
-    
-    //Bashkangjitja e një dëgjuesi(listener) për të marrë azhurnime sa herë që pika e fundit e sendeve të modifikohet.
-    ref.queryOrdered(byChild: "completed").observe(.value, with: { snapshot in
-      var newItems: [TodoItem] = []//ruajtja e te dhenave ne variablen lokale sa here qe ndryshojne
-        
-      //Struktura TodoItem ka nje iniciator qe ruat vetite e tij duke përdorur një DataSnapshot. Vlera snapshot eshte e llojit AnyObject dhe mund te jete nje fjalor, varg, numer ose varg. Pas krijimit te një shembulli të TodoItem, e shton atë në varg qe permban versionin e fundit të të dhënave.
-      for child in snapshot.children {
-        if let snapshot = child as? DataSnapshot,
-          let todoItem = TodoItem(snapshot: snapshot) {
-          newItems.append(todoItem)
+//Krijon nje vezhgues që dëgjon per te dhenat e shtuara ne userRef.
+    usersRef.observe(.childAdded, with: { snap in
+      //Merr vleren e snapshot dhe e vendos ate ne vargun lokal currentUsers
+      guard let email = snap.value as? String else { return }
+      self.currentUsers.append(email)
+      let row = self.currentUsers.count - 1// rreshti eshte gjithmone numri i vargut -1 sepse vargu fillon te numerohet nga 0
+      let indexPath = IndexPath(row: row, section: 0)//Krijimi i instances NSIndexPath duke perdorur indeksin e rreshtit llogaritur.
+      self.tableView.insertRows(at: [indexPath], with: .top)// Inserton rreshtin duke përdorur një animacion që bën që rreshti të insertohet nga lart.
+    })
+    //Perdorja e aplikacionit nga perdoruesit te cilet nuk kane lidhje interneti
+    usersRef.observe(.childRemoved, with: { snap in
+      guard let emailToFind = snap.value as? String else { return }
+      for (index, email) in self.currentUsers.enumerated() {
+        if email == emailToFind {
+          let indexPath = IndexPath(row: index, section: 0)
+          self.currentUsers.remove(at: index)
+          self.tableView.deleteRows(at: [indexPath], with: .fade)
         }
       }
-      //Barazimi i te dhenave me vargun e krijuar lokalisht ne verzionin e fundit te te dhenave nga databaza
-      self.items = newItems
-       //Refreshimi i tabeles
-      self.tableView.reloadData()
-    })
-
-    Auth.auth().addStateDidChangeListener { auth, user in
-      guard let user = user else { return }
-      self.user = User(authData: user)
-      
-      let currentUserRef = self.usersRef.child(self.user.uid)
-      currentUserRef.setValue(self.user.email)
-      currentUserRef.onDisconnectRemoveValue()
-    }
-    //metoda per numerimin e userave
-    usersRef.observe(.value, with: { snapshot in
-      if snapshot.exists() {// nese ekzistojne usera
-        self.userCountBarButtonItem?.title = snapshot.childrenCount.description // numerimi i userave dhe vendosja e numrit ne titull te butonit te krijuar manualisht
-      } else {
-        // nese nuk gjindet asnje user vendos vleren "0"
-        self.userCountBarButtonItem?.title = "0"
-      }
     })
   }
   
-  // MARK: UITableView Delegate methods
-//Metodat per funksionalitetin e tabelave
-    
-  //Numri i rreshtave në tabele
+//Numri i rreshtave në tabele
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count // Numerimi i listes
+    return currentUsers.count // Numerimi i listes
   }
-  
-    //Krijimi i rrreshtave ne baze te te dhenave ne vargun Items
+  //Krijimi i rreshtave ne baze te te dhenave ne vargun currentUsers
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
-    let todoItem = items[indexPath.row]
+    let cell = tableView.dequeueReusableCell(withIdentifier: userCell, for: indexPath)
+    let onlineUserEmail = currentUsers[indexPath.row] //indeksimi per qdo rresht ne tabele
     //vendosja e vlerave ne labela, ne baze te vargut te te dhenave Item
-    cell.textLabel?.text = todoItem.name
-    cell.detailTextLabel?.text = todoItem.addedByUser
-    
-    toggleCellCheckbox(cell, isCompleted: todoItem.completed)
-    
+    cell.textLabel?.text = onlineUserEmail
     return cell
   }
   
-  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    return true
-  }
-  // Funksioni i cili na lejo me e fshi items ne tabele duke bere swipe
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-            if editingStyle == .delete {
-          let todoItem = items[indexPath.row]// varesisht se cilin rresht duam ta fshijme, mirret index i rreshtit
-          todoItem.ref?.removeValue() // fshirja e te dhenes nga lista e te dhenave ne databaze
-        }
-
+  // MARK: Actions
+  @IBAction func signoutButtonPressed(_ sender: AnyObject) {
+    let user = Auth.auth().currentUser!
+    let onlineRef = Database.database().reference(withPath: "online/\(user.uid)")
+    onlineRef.removeValue { (error, _) in
+      if let error = error {
+        print("Removing online failed: \(error)")
+        return
+      }
+      do {
+        try Auth.auth().signOut()
+        self.dismiss(animated: true, completion: nil)
+      } catch (let error) {
+        print("Auth sign out failed: \(error)")
+      }
     }
-  //didSelectRowAt eshte funksionaliteti kur te selektojme nje rresht ne tabele
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    guard let cell = tableView.cellForRow(at: indexPath) else { return }
-    let todoItem = items[indexPath.row]// marrja e indeksit te cilin rresht e selektojme
-    let toggledCompletion = !todoItem.completed
-    toggleCellCheckbox(cell, isCompleted: toggledCompletion) // thirret funksioni per ta dizajni rreshtin me tik nese eshte e selektume
-    
-    // Editimi i te dhenave ne databaze, nese eshte rreshti i selektuar(kompletuar)
-    todoItem.ref?.updateChildValues([
-      "completed": toggledCompletion
-      ])
-  }
-  //Kjo metode ka per funksion te i jep dizajnin e rreshtit nese eshte apo jo i selektuar
-  func toggleCellCheckbox(_ cell: UITableViewCell, isCompleted: Bool) {
-    if !isCompleted {
-      cell.accessoryType = .none
-      cell.textLabel?.textColor = .black
-      cell.detailTextLabel?.textColor = .black
-    } else {
-      cell.accessoryType = .checkmark
-      cell.textLabel?.textColor = .gray
-      cell.detailTextLabel?.textColor = .gray
-    }
-  }
-  
-  // MARK: Insertimi i te dhenave  ne tabele
-    
-    //Eshte variabel e cila i jep ne funksionalitet butonit
-  @IBAction func addButtonDidTouch(_ sender: AnyObject) {
-    //Paraqitja e shtimit te te dhenave si alert(Pop-up)
-    let alert = UIAlertController(title: "TO DO",//titulli i alertit
-                                  message: "Add Task", // Udhezimi
-                                  preferredStyle: .alert) // forma e paraqitjes se alertit
-    //butoni Save ne Alert
-    let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
-        //Kontollimi nese ka te dhena ne fushat e tekstit(textfields), nese nuk ka nderpritet funksioni
-      guard let textField = alert.textFields?.first,
-        let text = textField.text else { return }
-      
-        //nese ka te dhena te shkruara ne fushat e tekstit(textfields) behet insertimi i te dhenave ne model
-      let todoItem = TodoItem(name: text,
-                                    addedByUser: self.user.email,
-                                    completed: false)
-
-      let todoItemRef = self.ref.child(text.lowercased()) // Shnderrimi i te dhenave hyrese nga textfields  me shkronja te vogla
-        
-      //bashkangjitja e te dhenes se referuar me larte ne databaze.
-      todoItemRef.setValue(todoItem.toAnyObject())
-    }
-    //Butoni Cancel ne  alert
-    let cancelAction = UIAlertAction(title: "Cancel",
-                                     style: .cancel)
-    
-    
-    alert.addTextField()
-    //Shtimi i dy butonave ne alert
-    alert.addAction(saveAction)
-    alert.addAction(cancelAction)
-    
-    present(alert, animated: true, completion: nil)
-  }
-  //Funksionaliteti per dergimin nga butoni i krijuar me lart userCountBarButtonItem per te derguar ne screen-in per user-a
-  @objc func userCountButtonDidTouch() {
-    performSegue(withIdentifier: listToUsers, sender: nil)
   }
 }
-
