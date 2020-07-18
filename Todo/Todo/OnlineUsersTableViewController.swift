b320f61c13f3fb025b24ff62dbbcfaed783ca7e8
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-07-19 00:13:18 +0200

    Update OnlineUsersTableViewController.swift

--- Todo/Todo/OnlineUsersTableViewController.swift
+++ Todo/Todo/OnlineUsersTableViewController.swift
@@ -20,24 +20,4 @@ class OnlineUsersTableViewController: UITableViewController {
     return .lightContent
   }
   
-  // MARK: UIViewController Lifecycle
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
   
-  // MARK: UITableView Delegate methods
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 2
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: userCell, for: indexPath)
-   
-    return cell
-  }
-  
-  // MARK: Actions
-  @IBAction func signoutButtonPressed(_ sender: AnyObject) {
-
-  }
-}
