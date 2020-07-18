c69b729e5167978676a15f20bbfbfe3f6a0a493c
Taulant Isaku <44265863+Taulant8@users.noreply.github.com>
2020-07-18 23:56:15 +0200

    Update TODOListTableViewController.swift

--- Todo/Todo/TODOListTableViewController.swift
+++ Todo/Todo/TODOListTableViewController.swift
@@ -29,18 +29,32 @@ class TODOListTableViewController: UITableViewController {
   // MARK: UIViewController Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    var userCountBarButtonItem: UIBarButtonItem!
-    tableView.allowsMultipleSelectionDuringEditing = false
     
+  tableView.allowsMultipleSelectionDuringEditing = false // Lejimi i selektimit te rreshtit ne tabele
+    // Bashkangjitja e funksionalitetit ne butonin per numerimin e userave
     userCountBarButtonItem = UIBarButtonItem(title: "1",
                                              style: .plain,
                                              target: self,
                                              action: #selector(userCountButtonDidTouch))
-    userCountBarButtonItem.tintColor = UIColor.white
-    navigationItem.leftBarButtonItem = userCountBarButtonItem
+    userCountBarButtonItem.tintColor = UIColor.white // Ngjyrosja e butonit ne navigation bar
+    navigationItem.leftBarButtonItem = userCountBarButtonItem // Vendosja butonit te krijuar manualisht(userCountBarButtonItem) ne butonin e navigimit leftbuttonItem
     
-    
-  }
+    //Bashkangjitja e një dëgjuesi(listener) për të marrë azhurnime sa herë që pika e fundit e sendeve të modifikohet.
+    ref.queryOrdered(byChild: "completed").observe(.value, with: { snapshot in
+      var newItems: [TodoItem] = []//ruajtja e te dhenave ne variablen lokale sa here qe ndryshojne
+        
+      //Struktura TodoItem ka nje iniciator qe ruat vetite e tij duke përdorur një DataSnapshot. Vlera snapshot eshte e llojit AnyObject dhe mund te jete nje fjalor, varg, numer ose varg. Pas krijimit te një shembulli të TodoItem, e shton atë në varg qe permban versionin e fundit të të dhënave.
+      for child in snapshot.children {
+        if let snapshot = child as? DataSnapshot,
+          let todoItem = TodoItem(snapshot: snapshot) {
+          newItems.append(todoItem)
+        }
+      }
+      //Barazimi i te dhenave me vargun e krijuar lokalisht ne verzionin e fundit te te dhenave nga databaza
+      self.items = newItems
+       //Refreshimi i tabeles
+      self.tableView.reloadData()
+    })
   // MARK: UITableView Delegate methods
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return 3
